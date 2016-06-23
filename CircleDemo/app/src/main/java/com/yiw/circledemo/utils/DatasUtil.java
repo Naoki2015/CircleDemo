aac64addc3087000a6cf049b662e115dec78662e
yiw <812160644@qq.com>
2016-06-23 11:26:14 +0800

    增加电话号匹配规则
    修改url匹配bug，增加电话号匹配规则

--- CircleDemo/app/src/main/java/com/yiw/circledemo/utils/DatasUtil.java
+++ CircleDemo/app/src/main/java/com/yiw/circledemo/utils/DatasUtil.java
@@ -18,9 +18,9 @@ import java.util.Random;
  */
 public class DatasUtil {
 	public static final String[] CONTENTS = { "",
-			"哈哈，VR013  http://www.vr013.com;一个不错的VR网站。哈哈，VR013  http://www.vr013.com;一个不错的VR网站。哈哈，VR013  http://www.vr013.com;一个不错的VR网站。哈哈，VR013  http://www.vr013.com;一个不错的VR网站。",
-			"今天是个好日子，http://www.vr013.com;一个不错的VR网站",
-			"呵呵，http://www.vr013.com;一个不错的VR网站",
+			"哈哈，18123456789,VR013  http://www.vr013.com;一个不错的VR网站。哈哈，VR013  http://www.vr013.com;一个不错的VR网站。哈哈，VR013  http://www.vr013.com;一个不错的VR网站。哈哈，VR013  http://www.vr013.com;一个不错的VR网站。",
+			"今天是个好日子，http://www.vr013.com;一个不错的VR网站,18123456789,",
+			"呵呵，http://www.vr013.com;一个不错的VR网站,18123456789,",
 			"只有http|https|ftp|svn://开头的网址才能识别为网址，正则表达式写的不太好，如果你又更好的正则表达式请评论告诉我，谢谢！",
 			"VR（Virtual Reality，即虚拟现实，简称VR），是由美国VPL公司创建人拉尼尔（Jaron Lanier）在20世纪80年代初提出的。其具体内涵是：综合利用计算机图形系统和各种现实及控制等接口设备，在计算机上生成的、可交互的三维环境中提供沉浸感觉的技术。其中，计算机生成的、可交互的三维环境称为虚拟环境（即Virtual Environment，简称VE）。虚拟现实技术是一种可以创建和体验虚拟世界的计算机仿真系统的技术。它利用计算机生成一种模拟环境，利用多源信息融合的交互式三维动态视景和实体行为的系统仿真使用户沉浸到该环境中。",
 			"哈哈哈哈",
--- CircleDemo/app/src/main/java/com/yiw/circledemo/utils/UrlUtils.java
+++ CircleDemo/app/src/main/java/com/yiw/circledemo/utils/UrlUtils.java
@@ -23,28 +23,50 @@ public class UrlUtils {
 
         SpannableStringBuilder sp;
         if(!TextUtils.isEmpty(contentStr)){
-            sp = new SpannableStringBuilder(contentStr);
-            Pattern pattern = Pattern.compile("(http|https|ftp|svn)://([a-zA-Z0-9]+[/?.?])" +
-                    "+[a-zA-Z0-9]*\\??([a-zA-Z0-9]*=[a-zA-Z0-9]*&?)*");
-            Matcher matcher = pattern.matcher(contentStr);
 
-            while (matcher.find()) {
-                final String url = matcher.group();
-                int start = contentStr.indexOf(url);
-                if (start >= 0) {
-                    int end = start + url.length();
-                    //sp.setSpan(new URLSpan(url), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    sp.setSpan(new SpannableClickable(){
-                        @Override
-                        public void onClick(View widget) {
-                            Uri uri = Uri.parse(url);
-                            Context context = widget.getContext();
-                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                            intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
-                            context.startActivity(intent);
-                        }
-                    }, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            sp = new SpannableStringBuilder(contentStr);
+            try {
+                //处理url匹配
+                Pattern urlPattern = Pattern.compile("(http|https|ftp|svn)://([a-zA-Z0-9]+[/?.?])" +
+                        "+[a-zA-Z0-9]*\\??([a-zA-Z0-9]*=[a-zA-Z0-9]*&?)*");
+                Matcher urlMatcher = urlPattern.matcher(contentStr);
+
+                while (urlMatcher.find()) {
+                    final String url = urlMatcher.group();
+                    if(!TextUtils.isEmpty(url)){
+                        sp.setSpan(new SpannableClickable(){
+                            @Override
+                            public void onClick(View widget) {
+                                Uri uri = Uri.parse(url);
+                                Context context = widget.getContext();
+                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+                                context.startActivity(intent);
+                            }
+                        }, urlMatcher.start(), urlMatcher.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }
                 }
+
+                //处理电话匹配
+                Pattern phonePattern = Pattern.compile("[1][34578][0-9]{9}");
+                Matcher phoneMatcher = phonePattern.matcher(contentStr);
+                while (phoneMatcher.find()) {
+                    final String phone = phoneMatcher.group();
+                    if(!TextUtils.isEmpty(phone)){
+                        sp.setSpan(new SpannableClickable(){
+                            @Override
+                            public void onClick(View widget) {
+                                Context context = widget.getContext();
+                                //用intent启动拨打电话
+                                Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:"+phone));
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                context.startActivity(intent);
+                            }
+                        }, phoneMatcher.start(), phoneMatcher.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
             }
         }else{
             sp = new SpannableStringBuilder();
