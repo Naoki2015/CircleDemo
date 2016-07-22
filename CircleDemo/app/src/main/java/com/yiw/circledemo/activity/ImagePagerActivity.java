5999f861a32ecc220c6d5cf5c34e5e4ed7321e44
lewis <lewisregister@163.com>
2016-07-23 01:54:31 +0800

    Resolving the IllegalArgumentException by replacing PhotoView dependency, and adding photo rotation feature.

--- CircleDemo/app/src/main/java/com/yiw/circledemo/activity/ImagePagerActivity.java
+++ CircleDemo/app/src/main/java/com/yiw/circledemo/activity/ImagePagerActivity.java
@@ -3,74 +3,90 @@ package com.yiw.circledemo.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bm.library.PhotoView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
 import com.yiw.circledemo.R;
+import com.yiw.circledemo.utils.ImageSize;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 /**
- * Created by yiw on 2016/1/6.
+ * Created by Lewis on 2016/7/22.
  */
 public class ImagePagerActivity extends Activity{
+
     public static final String INTENT_IMGURLS = "imgurls";
     public static final String INTENT_POSITION = "position";
     public static final String INTENT_IMAGESIZE = "imagesize";
-
+    ViewPager viewPager;
+    LinearLayout viewGroup;
+    ArrayList<String> imgUrls;
     private List<View> guideViewList = new ArrayList<View>();
-    private LinearLayout guideGroup;
-    public ImageSize imageSize;
-    private int startPos;
-    private ArrayList<String> imgUrls;
-
-
-    public static void startImagePagerActivity(Context context, List<String> imgUrls, int position, ImageSize imageSize){
-        Intent intent = new Intent(context, ImagePagerActivity.class);
-        intent.putStringArrayListExtra(INTENT_IMGURLS, new ArrayList<String>(imgUrls));
-        intent.putExtra(INTENT_POSITION, position);
-        intent.putExtra(INTENT_IMAGESIZE, imageSize);
-        context.startActivity(intent);
-
-    }
+     int startPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagepager);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        guideGroup = (LinearLayout) findViewById(R.id.guideGroup);
-
         getIntentData();
-
-        ImageAdapter mAdapter = new ImageAdapter(this);
-        mAdapter.setDatas(imgUrls);
-        mAdapter.setImageSize(imageSize);
-        viewPager.setAdapter(mAdapter);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        viewGroup=(LinearLayout)findViewById(R.id.guideGroup) ;
+        viewPager=(ViewPager) findViewById(R.id.pager);
+        viewPager.setPageMargin((int) (getResources().getDisplayMetrics().density * 15));
+        addGuideView(viewGroup, startPos, imgUrls);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                if(imgUrls==null) {return 0;}
+                else {return imgUrls.size();}
+            }
 
             @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view==object;
+            }
+            @Override
+            public Object instantiateItem(ViewGroup container, int position){
+                PhotoView view = new PhotoView(ImagePagerActivity.this);
+                view.enable();
+                view.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                //view.setImageResource(imgsId[position]);
+                DisplayImageOptions imageOptions= new DisplayImageOptions.Builder().bitmapConfig(Bitmap.Config.RGB_565)
+                        .showImageForEmptyUri(R.drawable.balloon_qupai_image).showImageOnFail(R.drawable.balloon_qupai_image).cacheInMemory(true).cacheOnDisk(true).build();
+                ImageLoader.getInstance().displayImage(imgUrls.get(position),view,imageOptions);
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        goBack();
+                    }
+                });
+                container.addView(view);
+                return view;
+            }
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        });
+        viewPager.setCurrentItem(startPos);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
 
             @Override
@@ -85,19 +101,27 @@ public class ImagePagerActivity extends Activity{
 
             }
         });
-        viewPager.setCurrentItem(startPos);
+    }
 
-        addGuideView(guideGroup, startPos, imgUrls);
+    public static void startImagePagerActivity(Context context, List<String> imgUrls, int position, ImageSize imageSize){
+        Intent intent = new Intent(context, ImagePagerActivity.class);
+        intent.putStringArrayListExtra(INTENT_IMGURLS, new ArrayList<String>(imgUrls));
+        intent.putExtra(INTENT_POSITION, position);
+        //intent.putExtra(INTENT_IMAGESIZE, imageSize);
+        context.startActivity(intent);
+    }
 
+    public void goBack(){
+        this.onBackPressed();
     }
 
     private void getIntentData() {
         startPos = getIntent().getIntExtra(INTENT_POSITION, 0);
         imgUrls = getIntent().getStringArrayListExtra(INTENT_IMGURLS);
-        imageSize = (ImageSize) getIntent().getSerializableExtra(INTENT_IMAGESIZE);
+        //imageSize = (ImageSize) getIntent().getSerializableExtra(INTENT_IMAGESIZE);
     }
 
-    private void addGuideView(LinearLayout guideGroup, int startPos, ArrayList<String> imgUrls) {
+    public void addGuideView(LinearLayout guideGroup, int startPos, ArrayList<String> imgUrls){
         if(imgUrls!=null && imgUrls.size()>0){
             guideViewList.clear();
             for (int i=0; i<imgUrls.size(); i++){
@@ -110,165 +134,6 @@ public class ImagePagerActivity extends Activity{
                 guideGroup.addView(view, layoutParams);
                 guideViewList.add(view);
             }
-        }
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        try{
-            return super.dispatchTouchEvent(ev);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    private static class ImageAdapter extends PagerAdapter{
-
-        private List<String> datas = new ArrayList<String>();
-        private LayoutInflater inflater;
-        private Context context;
-        private ImageSize imageSize;
-        private ImageView smallImageView = null;
-
-        public void setDatas(List<String> datas) {
-            if(datas != null )
-                this.datas = datas;
-        }
-        public void setImageSize(ImageSize imageSize){
-            this.imageSize = imageSize;
-        }
-
-        public ImageAdapter(Context context){
-            this.context = context;
-            this.inflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            if(datas == null) return 0;
-            return datas.size();
-        }
-
-
-        @Override
-        public Object instantiateItem(ViewGroup container, final int position) {
-            View view = inflater.inflate(R.layout.item_pager_image, container, false);
-            if(view != null){
-                final ImageView imageView = (ImageView) view.findViewById(R.id.image);
-
-                if(imageSize!=null){
-                    //预览imageView
-                    smallImageView = new ImageView(context);
-                    FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(imageSize.getWidth(), imageSize.getHeight());
-                    layoutParams.gravity = Gravity.CENTER;
-                    smallImageView.setLayoutParams(layoutParams);
-                    smallImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                    ((FrameLayout)view).addView(smallImageView);
-                }
-
-                //loading
-                final ProgressBar loading = new ProgressBar(context);
-                FrameLayout.LayoutParams loadingLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT);
-                loadingLayoutParams.gravity = Gravity.CENTER;
-                loading.setLayoutParams(loadingLayoutParams);
-                ((FrameLayout)view).addView(loading);
-
-                final String imgurl = datas.get(position);
-
-                Glide.with(context)
-                        .load(imgurl)
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)//缓存多个尺寸
-                        .thumbnail(0.1f)//先显示缩略图  缩略图为原图的1/10
-                        .error(R.drawable.ic_launcher)
-                        .into(new GlideDrawableImageViewTarget(imageView){
-                            @Override
-                            public void onLoadStarted(Drawable placeholder) {
-                                super.onLoadStarted(placeholder);
-                               /* if(smallImageView!=null){
-                                    smallImageView.setVisibility(View.VISIBLE);
-                                    Glide.with(context).load(imgurl).into(smallImageView);
-                                }*/
-                                loading.setVisibility(View.VISIBLE);
-                            }
-
-                            @Override
-                            public void onLoadFailed(Exception e, Drawable errorDrawable) {
-                                super.onLoadFailed(e, errorDrawable);
-                                /*if(smallImageView!=null){
-                                    smallImageView.setVisibility(View.GONE);
-                                }*/
-                                loading.setVisibility(View.GONE);
-                            }
-
-                            @Override
-                            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
-                                super.onResourceReady(resource, animation);
-                                loading.setVisibility(View.GONE);
-                                /*if(smallImageView!=null){
-                                    smallImageView.setVisibility(View.GONE);
-                                }*/
-                            }
-                        });
-
-                container.addView(view, 0);
-            }
-            return view;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view.equals(object);
-        }
-
-        @Override
-        public void restoreState(Parcelable state, ClassLoader loader) {
-        }
-
-        @Override
-        public Parcelable saveState() {
-            return null;
-        }
-
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        guideViewList.clear();
-        super.onDestroy();
-    }
-
-    public static class ImageSize implements Serializable{
-
-        private int width;
-        private int height;
-
-        public ImageSize(int width, int height){
-            this.width = width;
-            this.height = height;
-        }
-
-        public int getHeight() {
-            return height;
-        }
-
-        public void setHeight(int height) {
-            this.height = height;
-        }
-
-        public int getWidth() {
-            return width;
-        }
-
-        public void setWidth(int width) {
-            this.width = width;
         }
     }
 }
