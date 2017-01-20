edc02ca07de5544084d82711b975b45a7bf1df4e
yiw <812160644@qq.com>
2017-01-20 11:36:39 +0800

    modify

--- CircleDemo/app/src/main/java/com/yiw/circledemo/MyApplication.java
+++ CircleDemo/app/src/main/java/com/yiw/circledemo/MyApplication.java
@@ -3,6 +3,7 @@ package com.yiw.circledemo;
 import android.app.Application;
 import android.content.Context;
 import android.os.Environment;
+import android.support.multidex.MultiDex;
 
 import com.yiw.qupai.QPManager;
 
@@ -28,7 +29,7 @@ public class MyApplication extends Application {
 		//LeakCanary.install(this);
         QPManager.getInstance(getApplicationContext()).initRecord();
 
-
+		MultiDex.install(this);
 	}
 
 	public static Context getContext(){
